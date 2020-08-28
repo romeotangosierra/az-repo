ca01f141f1931c3f754f3cbe9d448bac674937d2
romeotangosierra <70332390+romeotangosierra@users.noreply.github.com>
2020-08-28 22:47:11 +0800

    Update web_install.sh

--- web_install.sh
+++ web_install.sh
@@ -1,4 +1,4 @@
 {
-  "fileUris": ["https://raw.githubusercontent.com/romeotangosierra/az-repo/web_install.sh"],
+  "fileUris": ["https://raw.githubusercontent.com/romeotangosierra/az-repo/master/web_install.sh"],
   "commandToExecute": "./web_install.sh"
 }
